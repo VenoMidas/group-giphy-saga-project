5b2900e3adfa14b6f8de05c91fec5852a122cc65
Mark Schumacher <schuma1022@gmail.com>
2022-10-11 17:11:37 -0500

    Add dispatch and redux variable array to search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useState } from 'react'; // useState for local state
+import { useDispatch, useSelector } from 'react-redux'; // for redux
 
 function Search() {
     const [tag, setTag] = useState('');
+    const searchResults = useSelector(store => store.giphyList); // store giphyList from redux
+    const dispatch = useDispatch(); // setup dispatch
 
     const returnSearchResults = () => {
-        
-    }
+        dispatch({ type: 'FETCH_MOVIES', payload: tag });
+    };
 
     return (
         <div>
