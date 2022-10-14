0d1ff3a66b6e4f5ca88ae2c6ca22e011e6e8eb51
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-10-14 17:27:06 -0500

    Add Header component and Links to Route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import Header from '../Header/Header';
 import Search from '../Search/Search';
 import Favorite from '../Favorite/Favorite';
 
@@ -6,14 +7,18 @@ function App() {
   return (
     <div className="App">
       <Router>
-        {/* Home/Search Page */}
-        <Route path="/" exact>
-          <Search />
-        </Route>
-        {/* Favorites Page */}
-        <Route path="/favorite" exact>
-          <Favorite />
-        </Route>
+        <Header />
+        <br />
+        <div>
+          {/* Home/Search Page */}
+          <Route path="/" exact>
+            <Search />
+          </Route>
+          {/* Favorites Page */}
+          <Route path="/favorite" exact>
+            <Favorite />
+          </Route>
+        </div>
       </Router>
     </div>
   );
