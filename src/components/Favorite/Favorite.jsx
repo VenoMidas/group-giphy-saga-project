412699e38f74d0896125452a439c135823b8d0ec
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-10-14 19:08:41 -0500

    framework for Favorite component

---
+++ src/components/Favorite/Favorite.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function Favorite() {
+
+    const dispatch = useDispatch();
+
+    const favoriteArray = useSelector(store => store.favoriteList);
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_FAVORITE' }) // triggers saga then calls *fetchFavorite()
+    })
+
+
+    return (
+        <div>
+            <section className="favGifs">
+
+                <h2>FAVORITE GIFS</h2>
+
+                {favoriteArray.map(favGif => {
+
+                    return (
+                        <div key={favGif.id} >
+                            {/* url will need to be set when favorites table is created */}
+                            <img src={favGif.url} />  
+
+                        </div>
+                    );
+                })}
+            </section>
+        </div>
+    )
+
+
+
+
+
+
+}
+
+export default Favorite;
